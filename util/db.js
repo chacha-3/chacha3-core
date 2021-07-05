9cebfbc7a9e95ab45e40bb7ccb0a7cb90c3f41c7
Wei-Ming Thor <twm213@hotmail.com>
2021-07-06 01:12:48 +0800

    Fix manual running test using wrong DB name

--- util/db.js
+++ util/db.js
@@ -1,7 +1,11 @@
 const level = require('level');
 const sub = require('subleveldown');
 
-const dbName = (process.env.NODE_ENV === 'test') ? 'testdata' : 'data';
+function runningManualTest() {
+  return process.argv[0].includes('node') && process.argv[1].includes('test');
+}
+
+const dbName = (process.env.NODE_ENV === 'test' || runningManualTest()) ? 'testdata' : 'data';
 
 const DB = level(dbName);
 const WalletDB = sub(DB, 'wallet');
