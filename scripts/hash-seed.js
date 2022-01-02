d36ab35d2a50b918e110035cdf2d8d3e3bbd8472
Wei-Ming Thor <twm213@hotmail.com>
2022-01-02 11:31:49 +0800

    Add vanity seed generate

---
+++ scripts/hash-seed.js
@@ -0,0 +1,23 @@
+const XXHash = require('xxhash');
+const crypto = require('crypto');
+
+const version = Buffer.from([0x00]);
+const zeroBuffer = Buffer.from(Array(20).fill(0x00));
+const fingerprint = zeroBuffer;
+
+let checksum = 0;
+for (let i = 0; true; i = crypto.randomBytes(4).readUIntBE(0, 4)) {
+  checksum = XXHash.hash(fingerprint, i, 'buffer').slice(-4);
+
+  if (checksum.equals(Buffer.from([0xde, 0xad, 0xde, 0xad]))) {
+    console.log('double', i.toString(16));
+    break;
+  }
+
+  if (checksum.equals(Buffer.from([0xdd, 0xee, 0xaa, 0xdd]))) {
+    console.log('double', i.toString(16));
+    // break;
+  }
+}
+
+// console.log(Buffer.concat([version, fingerprint, checksum].toString('hex')));
